04c73906c3c759c7f59f789ef6cdb75fb90da86e
Bors Alexandru <bors.alexandru2004@gmail.com>
2024-04-25 22:54:49 +0300

    added main Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogoNavComponent } from './logo-nav/logo-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogoNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
