9c6c1c0d20093df4a59ce9664c5341914b18533e
Bors Alexandru <bors.alexandru2004@gmail.com>
2024-05-17 13:07:23 +0300

    Fix: fix mobile canvas bug

--- src/app/header-bootstrap/header-bootstrap.component.ts
+++ src/app/header-bootstrap/header-bootstrap.component.ts
@@ -1,7 +1,7 @@
 import { NgClass } from '@angular/common';
-import { Component, inject, Input, TemplateRef, ViewEncapsulation  } from '@angular/core';
+import { Component, inject, TemplateRef, ViewChild, ViewEncapsulation, HostListener, } from '@angular/core';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { NgbOffcanvas, NgbOffcanvasOptions, NgbOffcanvasRef } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -13,18 +13,55 @@ import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
   encapsulation: ViewEncapsulation.None,
 })
 
-
-
 export class HeaderBootstrapComponent {
   isCollapsed: boolean = true;
+  
+  // Constructor
+  // -------------
+  // constructor(private offcanvasService: NgbOffcanvas) {}
+  constructor() {}
 
-  isOffcanvasCollapsed: boolean = true;
-
+  // Mobile canvas components
+  // -------------
+  @ViewChild('content', { static: true }) content!: TemplateRef<any>;
+  private offcanvasRef!: NgbOffcanvasRef;
   private offcanvasService = inject(NgbOffcanvas);
 
-	openEnd(content: TemplateRef<any>) {
-		this.offcanvasService.open(content, { panelClass: 'details-panel', backdropClass: 'details-panel-backdrop', position: 'start' });
-	}
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: Event) {
+    let width = 992;
+    if (window.innerWidth >= width) {
+      if(this.offcanvasRef){
+        this.dismissCanvas("Colse resize");
+      }
+    }
+  }
+
+  openCanvas() {
+    const options: NgbOffcanvasOptions = {
+      panelClass: 'details-panel', 
+      backdropClass: 'details-panel-backdrop', 
+      position: 'start'
+    };
+
+    this.offcanvasRef = this.offcanvasService.open(this.content, options);
+  }
+
+  closeCanvas() {
+    if (this.offcanvasRef) {
+      this.offcanvasRef.close('Close click');
+    }
+  }
+
+  dismissCanvas(reason: string) {
+    if (this.offcanvasRef) {
+      this.offcanvasRef.dismiss(reason);
+    }
+  }
+
+  // 
+  // -------------
+
 }
 
 
