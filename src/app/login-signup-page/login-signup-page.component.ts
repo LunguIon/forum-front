96ccda3af9a6a8d04ee334d7d07835027f178abd
Alexander B <75189288+Aziyck@users.noreply.github.com>
2024-05-27 09:07:03 +0300

    UI - Made home componenet (#5)
    * Fix: fexed pfp squash is canvas
    
    * fix, update: fixed icons, and added light theme support
    
    * fix: made isLightThemeOn var in AppComponnet private
    
    * added white theme toggle on welcome page
    
    * update: added modal, without image
    
    * Feature: made post-component and integrated into home component. Fix: added ids on header and login page

--- src/app/login-signup-page/login-signup-page.component.ts
+++ src/app/login-signup-page/login-signup-page.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, inject, TemplateRef, ViewEncapsulation, OnDestroy} from '@angular/core';
 import { LogoNavComponent } from '../logo-nav/logo-nav.component';
-import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';  
+import { FormsModule, NgForm, NgModel, ReactiveFormsModule } from '@angular/forms';  
 import { NgClass, NgIf } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { initializeConstellations } from '../utils/constelations';
 import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 import { AppComponent } from '../app.component';
 import { AuthenticationService } from '../service/authentication.service';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
+import { ElementRefService } from '../utils/element-ref.service';
 
 @Component({
   selector: 'app-login-signup-page',
   standalone: true,
-  imports: [LogoNavComponent, FormsModule, ReactiveFormsModule, NgClass, NgIf, HttpClientModule],
+  imports: [LogoNavComponent, FormsModule, ReactiveFormsModule, NgClass, NgIf, HttpClientModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './login-signup-page.component.html',
   styleUrl: './login-signup-page.component.scss',
   animations: [
@@ -36,14 +39,26 @@ import { HttpClientModule } from '@angular/common/http';
       transition('clicked => unclicked', [
       ])
     ])
-  ] 
+  ],
+  encapsulation: ViewEncapsulation.None, 
 })
 
-export class LoginSignupPageComponent implements OnInit{
+export class LoginSignupPageComponent implements OnInit, OnDestroy{
   
   // Constructor and innit
   // -------------
-  constructor(private route : ActivatedRoute, private appComponenet : AppComponent, private authService : AuthenticationService) { 
+  private elementRefSubscription!: Subscription;
+
+  private modalService = inject(NgbModal);
+  private modalRef!: NgbModalRef
+  private modalContainer!: ElementRef; 
+  @ViewChild('content', { static: true }) content!: TemplateRef<any>;
+  openModal() {
+		this.modalRef = this.modalService.open(this.content, {centered: true,  container: this.modalContainer.nativeElement});
+	}
+
+
+  constructor(private route : ActivatedRoute, private appComponenet : AppComponent, private authService : AuthenticationService, private elementRefService: ElementRefService) { 
     this.appComponenet.showHeaderAndFooter = false;
   }
   redirectToGoogle(): void{
@@ -53,15 +68,29 @@ export class LoginSignupPageComponent implements OnInit{
   
   @ViewChild('canvasElement', { static: true }) canvasElement: ElementRef | undefined;
   ngOnInit(): void {
+    this.modalContainer = this.elementRefService.getElementRef();
+    this.elementRefSubscription = this.elementRefService.elementRefChanged.subscribe(
+      (elementRef) => {
+        this.modalContainer = elementRef;
+      }
+    );
+
     this.route.data.subscribe(data => {
       this.isSignup = data['isSignup'];
     });
+
 
     if (this.canvasElement) {
       let colorPrimary : string = getComputedStyle(document.documentElement).getPropertyValue('--clr-primary');
       let colorPrimaryRGBVals : string = getComputedStyle(document.documentElement).getPropertyValue('--clr-primary-rgb-vals');
       const canvas: HTMLCanvasElement = this.canvasElement.nativeElement;
       initializeConstellations(canvas, colorPrimary, colorPrimary, colorPrimaryRGBVals, false);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.elementRefSubscription) {
+      this.elementRefSubscription.unsubscribe();
     }
   }
   
@@ -105,6 +134,7 @@ export class LoginSignupPageComponent implements OnInit{
         },
         error: (error) => {
           // Handle login error
+          this.openModal();
           console.error('Login error');
         }
       });
@@ -120,10 +150,20 @@ export class LoginSignupPageComponent implements OnInit{
         },
         error: (error) => {
           // Handle signup error
+          this.openModal();
           console.error('Signup error');
         }
       });
     }
+  }
+
+  reloadPage() {
+    window.location.reload();
+  }
+
+  closeModalAndReload(){
+    this.modalRef.close('Close click');
+    this.reloadPage();
   }
 
 }
