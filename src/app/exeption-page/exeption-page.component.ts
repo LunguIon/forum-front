788c1f0fe62d567e4031273ca8c373a5dee3074c
Bors Alexandru <bors.alexandru2004@gmail.com>
2024-06-06 16:38:45 +0300

    integrated light-theme for the 404 page

--- src/app/exeption-page/exeption-page.component.ts
+++ src/app/exeption-page/exeption-page.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-exeption-page',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, NgIf, NgClass],
   templateUrl: './exeption-page.component.html',
   styleUrl: './exeption-page.component.scss'
 })
 export class ExeptionPageComponent implements OnDestroy{
+  // Constructor and Innit
+  // ------------- 
+  isLightThemeOn: boolean = this.appComponent.isLightThemeOn;
   constructor(private appComponent: AppComponent){
     this.appComponent.showHeaderAndFooter = false;
   }
