cfd0a489328ca658e28ec72256a4b7c38efd5797
LunguIon <lion.24072004@gmail.com>
2024-06-11 11:24:49 +0300

    Added the method for retrieving topics and linked all page to backend

--- src/app/models/topicdto.model.ts
+++ src/app/models/topicdto.model.ts
@@ -1,8 +1,9 @@
+import { UserDTO } from "./UserDTO.model";
 import { User } from "./user.model";
 
 export interface TopicDTO {
     id: number;
     title: string;
     content: string;
-    user: User;
+    user: UserDTO;
   }
